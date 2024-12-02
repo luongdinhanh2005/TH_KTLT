3989083f1e586b3c159ca58da4d1f3976371f026
luongdinhanh2005 <dinhanh26102005@gmail.com>
2024-12-02 20:56:26 +0700

    4.4.py

---
+++ BÀI 4/4.4.py
@@ -0,0 +1,8 @@
+print("luong dinh anh")
+print("mssv235752021610008")
+ds = input("Nhập danh sách: ").split()
+print(ds)  # In cả dãy vừa nhập
+
+# In từng phần tử trên một dòng
+for item in ds:
+    print(item)
