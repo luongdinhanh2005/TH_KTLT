24c6a910ec2fd20282c3a2fb6404f65df0740a36
luongdinhanh2005 <dinhanh26102005@gmail.com>
2024-12-02 21:23:30 +0700

    5.1.py

---
+++ BÀI 5/5.1.py
@@ -0,0 +1,11 @@
+print("luong dinh anh")
+print("mssv235752021610008")
+import mymath
+
+a = 10
+b = 5
+
+print("Add:", mymath.add(a, b))
+print("Subtract:", mymath.subtract(a, b))
+print("Multiply:", mymath.multiply(a, b))
+print("Divide:", mymath.divide(a, b))
