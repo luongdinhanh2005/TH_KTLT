21da2b8fb6cf4bc63ea2e1ba6c3baa1220433954
luongdinhanh2005 <dinhanh26102005@gmail.com>
2024-12-02 20:29:20 +0700

    2.8.py

---
+++ BÀI 2/2.8.py
@@ -0,0 +1,11 @@
+print("luong dinh anh")
+print("mssv235752021610008")
+a, b = 1,2
+total = 0
+print (a,end=" ")
+while (a <=4000000-1):
+    if a % 2 == 0:
+        total +=a
+    a, b = b, a+b
+    print(a,end=" ")
+print("\n sum of prime numbers term in fibonacci series: ", total)
