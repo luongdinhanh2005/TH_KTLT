fcba7b7da9f9fa5aaf94d664eed777a8dd56466d
luongdinhanh2005 <dinhanh26102005@gmail.com>
2024-12-02 20:45:21 +0700

    3.7.py

---
+++ BÀI 3/3.7.py
@@ -0,0 +1,8 @@
+print("luong dinh anh")
+print("mssv235752021610008")
+def checkValue(n):
+    if n%2 == 0:
+        print("Day la mot so chan")
+    else:
+        print("Day la mot so le")
+checkValue(7
