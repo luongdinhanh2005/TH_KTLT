2fcf92cb5515052d1e2d80833de7deb73c087c7c
luongdinhanh2005 <dinhanh26102005@gmail.com>
2024-12-02 21:18:06 +0700

    4.23.py

---
+++ BÀI 4/4.23.py
@@ -0,0 +1,12 @@
+print("luong dinh anh")
+print("mssv235752021610008")
+# Nhập câu từ người dùng
+cau = input("Nhập một câu: ")
+
+# Đếm số chữ cái và số chữ số
+so_chu_cai = sum(ch.isalpha() for ch in cau)
+so_chu_so = sum(ch.isdigit() for ch in cau)
+
+# In kết quả
+print(f"Số chữ cái là: {so_chu_cai}")
+print(f"Số chữ số là: {so_chu_so}")
