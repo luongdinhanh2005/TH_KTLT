529a6390be2274c397d6594d61927b350d53e0fc
luongdinhanh2005 <dinhanh26102005@gmail.com>
2024-12-02 20:16:43 +0700

    2.1.py

---
+++ BÀI 2/2.1.py
@@ -0,0 +1,16 @@
+print("luong dinh anh")
+print("mssv235752021610008")
+#n1 = int (input("enter n1 value"))
+#n2 = int (input("enter n2 value"))
+#if n1>n2:
+#print("n1 is big")
+#else :
+#print("n2 is big")
+
+# Sửa lỗi
+n1 = int (input("enter n1 value"))
+n2 = int (input("enter n2 value"))
+if n1>n2:
+ print("n1 is big")
+else :
+ print("n2 is big")
