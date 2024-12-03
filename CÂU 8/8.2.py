f85c3c334046f969378ad07ce0cd9423961f041c
luongdinhanh2005 <dinhanh26102005@gmail.com>
2024-12-03 20:23:07 +0700

    8.2.py

---
+++ CÂU 8/8.2.py
@@ -0,0 +1,17 @@
+print("luong dinh anh")
+print("mssv235752021610008")
+import turtle
+
+def draw_circle():
+    window = turtle.Screen()
+    window.bgcolor("lightblue")
+
+    pen = turtle.Turtle()
+    pen.color("red")
+    pen.pensize(2)
+
+    pen.circle(50)
+
+    window.mainloop()
+
+draw_circle()
