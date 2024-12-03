af49fce70ffb7514199d777154ac79f7fdbd3445
luongdinhanh2005 <dinhanh26102005@gmail.com>
2024-12-03 20:06:43 +0700

    6.4.py

---
+++ BÀI 6/6.4.py
@@ -0,0 +1,30 @@
+print("luong dinh anh")
+print("mssv235752021610008")
+class RomanToInteger:
+    def __init__(self, roman):
+        self.roman = roman
+        self.roman_values = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+
+    def convert(self):
+        total = 0
+        prev_value = 0
+        for char in reversed(self.roman):
+            value = self.roman_values[char]
+            if value < prev_value:
+                total -= value
+            else:
+                total += value
+            prev_value = value
+        return total
+
+# Ví dụ sử dụng
+roman_numeral = RomanToInteger('XIV')
+print("Giá trị số nguyên:", roman_numeral.convert())
