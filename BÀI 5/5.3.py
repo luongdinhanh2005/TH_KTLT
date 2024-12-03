720a162da31a129c71dc5dc4f5867d0619f4ec80
luongdinhanh2005 <dinhanh26102005@gmail.com>
2024-12-03 19:53:42 +0700

    5.3.py

---
+++ BÀI 5/5.3.py
@@ -0,0 +1,6 @@
+import numpy as np
+print("luong dinh anh")
+print("mssv235752021610008")
+# Tạo mảng với các giá trị từ 12 đến 38
+array = np.arange(12, 39)
+print("Array with values from 12 to 38:", array)
