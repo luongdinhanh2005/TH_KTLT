a8cae5b476d8c64a3bd24f015887bba9b7904d4c
luongdinhanh2005 <dinhanh26102005@gmail.com>
2024-12-03 19:54:59 +0700

    5.4.py

---
+++ BÀI 5/5.4.py
@@ -0,0 +1,10 @@
+import numpy as np
+print("luong dinh anh")
+print("mssv235752021610008")
+# Tạo mảng với các giá trị từ 12 đến 38
+array = np.arange(12, 39)
+print("Original array:", array)
+
+# Đảo ngược mảng
+reversed_array = array[::-1]
+print("Reversed array:", reversed_array)
