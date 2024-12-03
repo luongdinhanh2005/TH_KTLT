8d763eb7e776790213f1882e95fe593d88c7fdd3
luongdinhanh2005 <dinhanh26102005@gmail.com>
2024-12-03 20:25:11 +0700

    8.4.py

---
+++ CÂU 8/8.4.py
@@ -0,0 +1,14 @@
+print("luong dinh anh")
+print("mssv235752021610008")
+import tkinter as tk
+
+def on_button_click():
+    print("Button clicked!")
+
+window = tk.Tk()
+window.title("My Window")
+
+button = tk.Button(window, text="Click Me", command=on_button_click, bg="blue", fg="white")
+button.pack(pady=20)
+
+window.mainloop()
